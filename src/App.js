b81266429b258e308b22d6f3f7e3940619ebb391
mannmanjeet988 <mannmanjeet988@gmail.com>
2024-07-15 03:53:20 -0700

    PAGE Visit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 
 
+// import { useEffect } from 'react';
+// import {useLocation, BrowserRouter,Route,Routes } from 'react-router-dom';
+// import './App.css';
+// import Header from './components/Common/Header';
+// import DashboardPage from "./pages/DashboardPage"
+// import MainComponent from './components/LandingPage/MainComponent';
+// import { Dashboard } from '@mui/icons-material';
+//  import HomePage from './pages/HomePage';
+// import CoinPage from './pages/CoinPage';
+// import ComparePage from './pages/ComparePage.js/index.js';
+// import { RWebShare } from 'react-web-share';
+// import WatchListPage from './pages/Watchlist/WatchListPage';
+// import { ToastContainer } from 'react-toastify';
+// import ReactGA from "react-ga4";
+
+
+// function App() {
+
+//   const location = useLocation();
+  
+//   useEffect(() => {
+//     ReactGA.initialize("G-61SDENNHNG");
+//   }, []);
+
+//   useEffect(() => {
+//     ReactGA.send({ hitType: "pageview", page: location.pathname, title: document.title });
+//   }, [location]);
+
+  
+//   return (
+//     <div className="App">
+      
+//       <ToastContainer />
+//       <BrowserRouter>
+//           <Routes>
+//             <Route path="/"  element ={<HomePage />} />
+//             <Route path="/dashboard" element ={<DashboardPage />} />
+//             <Route path="/coin/:id" element ={<CoinPage />} />
+//             <Route path="/compare" element ={<ComparePage />} />
+//             <Route path="/watchlist" element ={<WatchListPage />} />
+//           </Routes>
+//       </BrowserRouter>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useEffect } from 'react';
-import {useLocation, BrowserRouter,Route,Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Header from './components/Common/Header';
-import DashboardPage from "./pages/DashboardPage"
-import MainComponent from './components/LandingPage/MainComponent';
-import { Dashboard } from '@mui/icons-material';
- import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage';
+import DashboardPage from './pages/DashboardPage';
 import CoinPage from './pages/CoinPage';
 import ComparePage from './pages/ComparePage.js/index.js';
-import { RWebShare } from 'react-web-share';
 import WatchListPage from './pages/Watchlist/WatchListPage';
 import { ToastContainer } from 'react-toastify';
 import ReactGA from "react-ga4";
 
-
 function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+      <BrowserRouter>
+        <Header />
+        <RoutesComponent />
+      </BrowserRouter>
+    </div>
+  );
+}
 
+function RoutesComponent() {
   const location = useLocation();
-  
+
   useEffect(() => {
     ReactGA.initialize("G-61SDENNHNG");
   }, []);
@@ -29,20 +85,13 @@ function App() {
   }, [location]);
 
   return (
-    <div className="App">
-      
-      <ToastContainer />
-      <BrowserRouter>
-          <Routes>
-            <Route path="/"  element ={<HomePage />} />
-            <Route path="/dashboard" element ={<DashboardPage />} />
-            <Route path="/coin/:id" element ={<CoinPage />} />
-            <Route path="/compare" element ={<ComparePage />} />
-            <Route path="/watchlist" element ={<WatchListPage />} />
-          </Routes>
-      </BrowserRouter>
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/coin/:id" element={<CoinPage />} />
+      <Route path="/compare" element={<ComparePage />} />
+      <Route path="/watchlist" element={<WatchListPage />} />
+    </Routes>
   );
 }
 
