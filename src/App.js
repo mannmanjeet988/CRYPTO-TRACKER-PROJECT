052607e57813bf97b671081dd8a253df0692d553
mannmanjeet988 <mannmanjeet988@gmail.com>
2024-07-14 23:45:29 -0700

    added analtyics code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import {useLocation, BrowserRouter,Route,Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Common/Header';
 import DashboardPage from "./pages/DashboardPage"
@@ -12,10 +12,26 @@ import ComparePage from './pages/ComparePage.js/index.js';
 import { RWebShare } from 'react-web-share';
 import WatchListPage from './pages/Watchlist/WatchListPage';
 import { ToastContainer } from 'react-toastify';
+import GA4React from 'ga-4-react';
 
 
 function App() {
-  
+  // const location = useLocation();
+
+  // useEffect(() => {
+  //   ga4react.pageview(location.pathname + location.search);
+  // }, [location]);
+
+  const location = useLocation();
+  const GA4_MEASUREMENT_ID="G-C6GX6L1YXT"
+
+  useEffect(() => {
+    const ga4react = new GA4React('GA4_MEASUREMENT_ID');
+    ga4react.initialize().then(ga => {
+      ga.pageview(location.pathname + location.search);
+    });
+  }, [location]);
+
   return (
     <div className="App">
       
