7fecb2c3d66fe23323d19e9647c30d59a5d95096
mannmanjeet988 <mannmanjeet988@gmail.com>
2024-07-15 04:15:57 -0700

    error events added

--- src/App.js
+++ src/App.js
@@ -49,23 +49,24 @@
 
 // export default App;
 
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Header from './components/Common/Header';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage';
 import CoinPage from './pages/CoinPage';
-import ComparePage from './pages/ComparePage.js/index.js';
+import ComparePage from './pages/ComparePage';
 import WatchListPage from './pages/Watchlist/WatchListPage';
 import { ToastContainer } from 'react-toastify';
-import ReactGA from "react-ga4";
+import ReactGA from 'react-ga4';
 
 function App() {
   return (
     <div className="App">
       <ToastContainer />
       <BrowserRouter>
+        <Header />
         <RoutesComponent />
       </BrowserRouter>
     </div>
@@ -77,11 +78,33 @@ function RoutesComponent() {
 
   useEffect(() => {
     ReactGA.initialize("G-61SDENNHNG");
+    ReactGA.ga('set', 'debug_mode', true);  // Enable Debug Mode
+    console.log("GA initialized with ID:", "G-61SDENNHNG");
   }, []);
 
   useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: location.pathname, title: document.title });
+    ReactGA.send({ hitType: 'pageview', page: location.pathname, title: document.title });
+    console.log("Pageview sent:", { page: location.pathname, title: document.title });
   }, [location]);
+
+  const logErrorEvent = (error) => {
+    ReactGA.event({
+      category: 'Error',
+      action: 'An error occurred',
+      label: error.message,
+      nonInteraction: true,
+    });
+  };
+
+  // Example of how to log an error
+  useEffect(() => {
+    try {
+      // Simulate an error
+      throw new Error('This is a simulated error');
+    } catch (error) {
+      logErrorEvent(error);
+    }
+  }, []);
 
   return (
     <Routes>
